2eaedfbef1086102848e4fe46fe513cb9159afb4
Kirill Dubovitskiy <kirill2003de@gmail.com>
2023-11-07 02:37:28 -0800

    - Added a simple tool that adds two numbers to the assistant - Added a test for it - Converted tabs to spaces (I think the default cloudfare setup script uses tabs)

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,28 +6,51 @@ import { describe, expect, it, beforeAll, afterAll } from "vitest";
  * You can set breakpoints in both!
  */
 describe("Assistant can reply", () => {
-	beforeAll(async () => {
-		// We need to wait a bit to let the worker start
-		await new Promise((resolve) => setTimeout(resolve, 2_000));
-	})
+  beforeAll(async () => {
+    // We need to wait a bit to let the worker start
+    await new Promise((resolve) => setTimeout(resolve, 2_000));
+  })
 
-	beforeAll(async () => {
-		// TODO: Check the server is running run /health or something
-	});
+  beforeAll(async () => {
+    // TODO: Check the server is running run /health or something
+  });
 
-	it("I can get the assistant to say Hi to me", async () => {
-		const response = await fetch("http://localhost:8787/threads", {
-			method: "POST",
-		});
-		const threadJSON: any = await response.json();
-		const threadId = threadJSON.threadId;
+  // Sanity check, the assistant is able to reply to a very simple message
+  it("I can get the assistant to say Hi to me", async () => {
+    const response = await fetch("http://localhost:8787/threads", {
+      method: "POST",
+    });
+    const threadJSON: any = await response.json();
+    const threadId = threadJSON.threadId;
 
-		const assistantResponse = await fetch(`http://localhost:8787/threads/${threadId}/messages`, {
-			method: "POST",
-			body: 'Hi there! (Respond with just a "Hi", its a test message)',
-		});
-		const assistantResponseText = await assistantResponse.text();
+    const assistantResponse = await fetch(`http://localhost:8787/threads/${threadId}/messages`, {
+      method: "POST",
+      body: 'Hi there! (Respond with just a "Hi", its a test message)',
+    });
+    const assistantResponseText = await assistantResponse.text();
 
-		expect(assistantResponseText).toBe("Hi");
-	});
+    expect(assistantResponseText).toBe("Hi");
+  });
+
+  // Makes sure it actually uses the tool
+  it("Assistant can add two large numbers", async () => {
+    const num1 = Math.floor(Math.random() * 1e9);
+    const num2 = Math.floor(Math.random() * 1e9);
+
+    const response = await fetch("http://localhost:8787/threads", {
+      method: "POST",
+    });
+    const threadJSON: any = await response.json();
+    const threadId = threadJSON.threadId;
+
+    const assistantResponse = await fetch(`http://localhost:8787/threads/${threadId}/messages`, {
+      method: "POST",
+      body: `Add ${num1} and ${num2}. Reply only with the answer.`,
+    });
+    const assistantResponseText = await assistantResponse.text();
+
+    expect(assistantResponseText).toBe((num1 + num2).toString());
+  }, {
+    timeout: 20_000,
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,106 +2,182 @@ import { RequestLike, Router } from "itty-router";
 import { OpenAI } from "openai";
 
 interface Env {
-	OPENAI_API_KEY: string;
+  OPENAI_API_KEY: string;
 }
 
 // Hardcoded assistant details
-const ASSISTANT_NAME = "My Assistant";
+const ASSISTANT_NAME = "My Assistant with a custom tool to add two numbers";
 const ASSISTANT_MODEL = "gpt-4-1106-preview";
 const ASSISTANT_DESCRIPTION =
-	"A friendly assistant to help you with your queries.";
+  "A friendly assistant to help you with your queries.";
 
 // Router setup using itty-router
 const router = Router();
 
 // Endpoint to create a new thread
 router.post("/threads", async (request: RequestLike, openai: OpenAI) => {
-	const thread = await openai.beta.threads.create({});
-	return new Response(JSON.stringify({ threadId: thread.id }), {
-		headers: { "Content-Type": "application/json" },
-	});
+  const thread = await openai.beta.threads.create({});
+  return new Response(JSON.stringify({ threadId: thread.id }), {
+    headers: { "Content-Type": "application/json" },
+  });
 });
+
+// Your tool (a fancy name for a function)
+async function addNumbers(a: number, b: number): Promise<number> {
+  // An actual tool will likely do some asynchronous stuff, like calling an API
+  await new Promise((resolve) => setTimeout(resolve, 100));
+  return a + b;
+}
 
 // Endpoint to post a message to a thread and get a response from the assistant
 router.post(
-	"/threads/:threadId/messages",
-	async (request: RequestLike, openai: OpenAI) => {
-		const url = new URL(request.url);
-		const threadId = url.pathname.split("/")[2]; //
-		const requestBody = await request.text();
+  "/threads/:threadId/messages",
+  async (request: RequestLike, openai: OpenAI) => {
+    const url = new URL(request.url);
+    const threadId = url.pathname.split("/")[2]; //
+    const requestBody = await request.text();
 
-		// Retrieve or create the assistant
-		let assistant = (await openai.beta.assistants.list()).data.find(
-			(a) => a.name === ASSISTANT_NAME
-		);
-		if (!assistant) {
-			assistant = await openai.beta.assistants.create({
-				name: ASSISTANT_NAME,
-				model: ASSISTANT_MODEL,
-				instructions: ASSISTANT_DESCRIPTION,
-			});
-		}
+    // Retrieve or create the assistant
+    let assistant = (await openai.beta.assistants.list()).data.find(
+      (a) => a.name === ASSISTANT_NAME
+    );
+    if (!assistant) {
+      assistant = await openai.beta.assistants.create({
+        name: ASSISTANT_NAME,
+        model: ASSISTANT_MODEL,
+        instructions: ASSISTANT_DESCRIPTION,
+        // Adding an example tool
+        tools: [
+          {
+            type: "function",
+            function: {
+              name: "addNumbers",
+              description: "Add two numbers",
+              parameters: {
+                type: "object",
+                properties: {
+                  a: { type: "number", description: "First number" },
+                  b: { type: "number", description: "Second number" },
+                },
+                required: ["a", "b"],
+              },
+            },
+          },
+        ],
+      });
+    }
 
-		// Create a message in the thread
-		await openai.beta.threads.messages.create(threadId, {
-			role: "user",
-			content: requestBody,
-		});
+    // Create a message in the thread
+    await openai.beta.threads.messages.create(threadId, {
+      role: "user",
+      content: requestBody,
+    });
 
-		// Run the assistant on the thread to get a response
-		const run = await openai.beta.threads.runs.create(threadId, {
-			assistant_id: assistant.id,
-		});
+    // Run the assistant on the thread to get a response
+    const run = await openai.beta.threads.runs.create(threadId, {
+      assistant_id: assistant.id,
+    });
 
-		// Poll for the completion of the run
-		let completedRun;
-		do {
-			await new Promise((resolve) => setTimeout(resolve, 1000)); // Delay for 1 second
-			completedRun = await openai.beta.threads.runs.retrieve(threadId, run.id);
-		} while (completedRun.status !== "completed");
+    // Poll for the completion of the run
+    let completedRun;
+    do {
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // Delay for 1 second
+      completedRun = await openai.beta.threads.runs.retrieve(threadId, run.id);
 
-		// Retrieve the messages added by the Assistant to the Thread after the Run completes
-		const messages = await openai.beta.threads.messages.list(threadId);
-		const lastMessage = messages.data.at(0)
-		if (lastMessage?.role !== "assistant") {
-			return new Response("Last message not from the assistant", {
-				headers: { "Content-Type": "text/plain" },
-			});
-		}
+      // KEY Check if status requires action then run our custom function
+      if (
+        completedRun.status === "requires_action" &&
+        completedRun.required_action
+      ) {
+        const toolCalls =
+          completedRun.required_action.submit_tool_outputs.tool_calls;
+        for (const toolCall of toolCalls) {
+          if (toolCall.function.name === "addNumbers") {
+            // KEY Actually call the tool
 
-		const assistantMessageContent = lastMessage.content.at(0)
-		if (!assistantMessageContent) {
-			return new Response("No assistant message found", {
-				headers: { "Content-Type": "text/plain" },
-			});
-		}
+            // TODO: Add zod validation later
+            const args = JSON.parse(toolCall.function.arguments);
+            const result = await addNumbers(args.a, args.b);
 
-		if (assistantMessageContent.type !== 'text') {
-			return new Response("Assistant message is not text, only text supported in this demo", {
-				headers: { "Content-Type": "text/plain" },
-			});
-		}
+            // KEY Submit the output back to the run
+            await openai.beta.threads.runs.submitToolOutputs(threadId, run.id, {
+              tool_outputs: [
+                {
+                  tool_call_id: toolCall.id,
+                  output: JSON.stringify({ result }),
+                },
+              ],
+            });
+          }
+        }
 
-		// Return the assistant's response
-		return new Response(assistantMessageContent.text.value, {
-			headers: { "Content-Type": "text/plain" },
-		});
-	}
+        // Continue polling for completion
+        continue;
+      }
+
+      // Return if the run appears dead
+      if (
+        completedRun.status === "cancelled" ||
+        completedRun.status === "cancelling" ||
+        completedRun.status === "failed" ||
+        completedRun.status === "expired"
+      ) {
+        return new Response(
+          `Run stopped due to status: ${completedRun.status}`,
+          {
+            headers: { "Content-Type": "text/plain" },
+          }
+        );
+      }
+    } while (completedRun.status !== "completed");
+
+    // Retrieve the messages added by the Assistant to the Thread after the Run completes
+    const messages = await openai.beta.threads.messages.list(threadId);
+
+    // A bunch of boring safety checks
+    const lastMessage = messages.data.at(0);
+    if (lastMessage?.role !== "assistant") {
+      return new Response("Last message not from the assistant", {
+        headers: { "Content-Type": "text/plain" },
+      });
+    }
+
+    const assistantMessageContent = lastMessage.content.at(0);
+    if (!assistantMessageContent) {
+      return new Response("No assistant message found", {
+        headers: { "Content-Type": "text/plain" },
+      });
+    }
+
+    if (assistantMessageContent.type !== "text") {
+      return new Response(
+        "Assistant message is not text, only text supported in this demo",
+        {
+          headers: { "Content-Type": "text/plain" },
+        }
+      );
+    }
+
+    // Return the assistant's response
+    return new Response(assistantMessageContent.text.value, {
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
 );
 
 router.all("*", () => new Response("Not found", { status: 404 }));
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		// Set up OpenAI configuration with the API key
-		const openai = new OpenAI({
-			apiKey: env.OPENAI_API_KEY,
-		});
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    // Set up OpenAI configuration with the API key
+    const openai = new OpenAI({
+      apiKey: env.OPENAI_API_KEY,
+    });
 
-		return router.handle(request, openai);
-	},
+    return router.handle(request, openai);
+  },
 };
